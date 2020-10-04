2370100b5d7f47ed58f28c46fe9b265c11ddec07
sazanca <moriya.kz817@gmail.com>
2020-10-05 01:08:02 +0900

    fix puma

--- config/puma.rb
+++ config/puma.rb
@@ -10,11 +10,12 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV['PORT'] || 3000 
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+# environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV['RACK_ENV'] || 'development'
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -43,7 +44,7 @@ threads threads_count, threads_count
 
 preload_app!
 
-rackup      DefaultRackup
+# rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
--- db/migrate/20201004132011_create_profiles.rb
+++ db/migrate/20201004132011_create_profiles.rb
@@ -1,8 +1,10 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-      t.text :shop
+      t.text :name
+      t.text :ganre
       t.integer :user_id
+      t.boolean :is_valid, default: true#クーポン有効性の真偽
       t.timestamps
     end
   end
